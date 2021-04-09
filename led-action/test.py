9fa9c86400df9ac7a9db97e89ab8ef5742e503ca
Yannick Lang <layaxx@gmx.de>
2021-04-09 17:38:24 +0200

    add led-action/test

---
+++ led-action/test.py
@@ -0,0 +1,17 @@
+import board
+import neopixel
+import time
+import sys
+pixels = neopixel.NeoPixel(board.D18, 60)
+
+try:
+    for _ in range(10):
+        for x in range(0, 50):
+            pixels[x] = (200, 0, 0)
+            time.sleep(0.1)
+        for y in range(50):
+            pixels[50-y] = (0, 0, 0)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pixels.deinit()
+    sys.exit(0)
